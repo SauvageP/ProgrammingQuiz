1d7d29f9d3a6dc4f88f8161a28a1436ae74d4b14
Perry <Perry@Perrys-MacBook-Pro.local>
2014-11-24 00:25:39 -0500

    First Commit
    Written in C#

---
+++ ProgrammingQuiz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProgrammingQuiz
+{
+	class ProblemOne
+	{
+		public static void Main (string[] args)
+		{
+			HelloWorld ();
+		}
+		public static void HelloWorld()
+		{
+			Console.WriteLine ("What is your name? ");
+			String name = Console.ReadLine ();
+			Console.WriteLine ("Hello World!! My name is " + name);
+		}
+	}
+}
