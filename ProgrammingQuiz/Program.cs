98b9901be8476cec4265e7004624f1705bb105b2
Perry <Perry@Perrys-MacBook-Pro.local>
2014-11-24 00:40:45 -0500

    Fixes
    Changed some names around and added a new method.

--- ProgrammingQuiz/Program.cs
+++ ProgrammingQuiz/Program.cs
@@ -2,17 +2,35 @@
 
 namespace ProgrammingQuiz
 {
-	class ProblemOne
+	class Problems
 	{
 		public static void Main (string[] args)
 		{
-			HelloWorld ();
+		//	HelloWorld ();
+			ProgramLang ();
 		}
+
 		public static void HelloWorld()
 		{
+			// Write code that will take a user's name and display it on console.
 			Console.WriteLine ("What is your name? ");
 			String name = Console.ReadLine ();
 			Console.WriteLine ("Hello World!! My name is " + name);
+		}
+
+		public static void ProgramLang()
+		{
+			Console.WriteLine ("What is your favorite programming languages? (CS, CPP, JAVA, C, etc.)");
+			String choice = Console.ReadLine ().ToUpper ();
+			switch (choice) {
+				case "CS":
+					Console.Write ("C #");
+					break;
+
+			default:
+				Console.Write ("Wrong Letters");
+				break;
+			}
 		}
 	}
 }
